34fc39e8b4d485b408dbd1804f2e3347f5f09626
Ashish jha <ajha5645@gmail.com>
2025-08-07 00:43:29 +0530

    add Patient entity and update project configuration

---
+++ patient-service/src/main/java/com/patientmanagement/patientservice/model/Patient.java
@@ -0,0 +1,8 @@
+package com.patientmanagement.patientservice.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "patients")
+public class Patient {
+}
