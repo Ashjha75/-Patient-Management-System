fca0982ff7669251ee56b36b62ede27faf008113
Ashish jha <ajha5645@gmail.com>
2025-08-08 18:46:06 +0530

    add Patient model with validation and unique constraints

--- patient-service/src/main/java/com/patientmanagement/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/model/Patient.java
@@ -1,8 +1,57 @@
 package com.patientmanagement.patientservice.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 @Table(name = "patients")
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 3, max = 20)
+    private  String username;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private String addressLine1;
+
+    private String addressLine2;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    @NotNull
+    private String country;
+
+    @NotNull
+    private String postalCode;
+
 }
