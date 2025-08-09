3ee2f07f861c8d9857e4786dc4d554e214f75e56
Ashish jha <ajha5645@gmail.com>
2025-08-09 13:35:34 +0530

    refactor PatientService to define interface and implement service logic in PatientServiceImpl

--- patient-service/src/main/java/com/patientmanagement/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/service/PatientService.java
@@ -1,29 +1,12 @@
 package com.patientmanagement.patientservice.service;
 
-import com.patientmanagement.patientservice.model.Patient;
+import com.patientmanagement.patientservice.dto.PatientRequestDto;
 import com.patientmanagement.patientservice.dto.PatientResponseDTO;
-import com.patientmanagement.patientservice.repository.PatientRepository;
-import com.patientmanagement.patientservice.mapper.PatientMapper;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class PatientService {
-    private final PatientRepository patientRepository;
+public interface PatientService {
+    List<PatientResponseDTO> getAllPatients();
 
-    public PatientService(PatientRepository patientRepository) {
-        this.patientRepository = patientRepository;
-    }
-
-    public List<PatientResponseDTO> getAllPatients() {
-        List<Patient> patients = patientRepository.findAll();
-     System.out.println("Fetching all patients from the database..." + patients.size()); // Debugging statement
-        return patients.stream()
-                .map(PatientMapper::toDTO) // Using method reference for better readability
-                .toList();
-//        return patients.stream()
-//                .map(patient -> PatientMapper.toDTO(patient)) // Assuming PatientMapper has a static method to convert Patient to PatientResponseDTO
-//                .toList();
-    }
+    PatientResponseDTO addPatient(PatientRequestDto patientRequestDto);
 }
---
+++ patient-service/src/main/java/com/patientmanagement/patientservice/serviceImplementation/PatientServiceImpl.java
@@ -0,0 +1,43 @@
+package com.patientmanagement.patientservice.serviceImplementation;
+
+import com.patientmanagement.patientservice.model.Patient;
+import com.patientmanagement.patientservice.dto.PatientResponseDTO;
+import com.patientmanagement.patientservice.dto.PatientRequestDto;
+import com.patientmanagement.patientservice.repository.PatientRepository;
+import com.patientmanagement.patientservice.mapper.PatientMapper;
+import com.patientmanagement.patientservice.service.PatientService;
+import com.patientmanagement.patientservice.util.IdGenerator;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public List<PatientResponseDTO> getAllPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        System.out.println("Fetching all patients from the database..." + patients.size());
+        return patients.stream()
+                .map(PatientMapper::toDTO)
+                .toList();
+    }
+
+    @Override
+    public PatientResponseDTO addPatient(PatientRequestDto patientRequestDto) {
+//        if (patientRepository.existsByUsername(patientRequestDto.getUsername())) {
+//            throw new IllegalArgumentException("Username already exists");
+//        }
+//        if (patientRepository.existsByEmail(patientRequestDto.getEmail())) {
+//            throw new IllegalArgumentException("Email already exists");
+//        }
+        String id = IdGenerator.generatePatientId();
+        Patient patient = PatientMapper.toModel(patientRequestDto, id);
+        Patient savedPatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(savedPatient);
+    }
+}
