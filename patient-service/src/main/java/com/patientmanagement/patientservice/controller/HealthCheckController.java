27c2bb039ecb4b8a47ab690bc8f57a422df70d5e
Ashish jha <ajha5645@gmail.com>
2025-08-09 11:18:44 +0530

    add Swagger annotations to HealthCheckController and PatientController for API documentation

--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/HealthCheckController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package com.patientmanagement.patientservice.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "1. Health Check",description = "Health check for Patient Service")
 public class HealthCheckController {
 
     @GetMapping("/health")
--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
@@ -1,8 +1,13 @@
 package com.patientmanagement.patientservice.controller;
 
+import com.patientmanagement.patientservice.dto.PatientRequestDto;
 import com.patientmanagement.patientservice.dto.PatientResponseDTO;
+import com.patientmanagement.patientservice.repository.PatientRepository;
 import com.patientmanagement.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +15,30 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "2. Patient")
 public class PatientController {
 
     private final PatientService patientService;
+    private final PatientRepository patientRepository;
 
-    public PatientController(PatientService patientService) {
+    public PatientController(PatientService patientService, PatientRepository patientRepository) {
         this.patientService = patientService;
+        this.patientRepository = patientRepository;
     }
 
     // Get all patients controller method
-    @RequestMapping("/all-patients")
+    @GetMapping("/all-patients")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
         List<PatientResponseDTO> patients = patientService.getAllPatients();
         return ResponseEntity.ok(patients);
+    }
+
+//    add patient Controller method
+
+    @PostMapping("/add-patient")
+    public PatientResponseDTO addPatient(PatientRequestDto patientRequestDto) {
+        Patient patient = patientRepository.save(
+                patientRequestDto.toModel(patientRequestDto);
+        )
     }
 }
