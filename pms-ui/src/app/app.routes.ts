3f399c22aa6d537fa2147ac39bb8f593dbd775f6
Ashish jha <ajha5645@gmail.com>
2025-08-14 09:58:47 +0530

    enhance CorrelationIdFilter and ProblemDetailBuilder with detailed documentation for request tracking and error response consistency

--- pms-ui/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-	{
-		path: 'get-users',
-		loadComponent: () => import('./administration/get-users/get-users.component').then(m => m.GetUsersComponent)
-	}
-];
