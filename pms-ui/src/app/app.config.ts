1fb1577168930c11d7e082fede0266e7b54df547
Ashish jha <ajha5645@gmail.com>
2025-08-14 09:59:05 +0530

    enhance CorrelationIdFilter and ProblemDetailBuilder with detailed documentation for request tracking and error response consistency

--- pms-ui/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: HTTP_INTERCEPTORS, useClass: ErrorHandler, multi: true }]
-};
