1f2731dcbad47be6fc27d2fa0af5cef6252203ed
Ashish jha <ajha5645@gmail.com>
2025-08-18 13:46:19 +0530

    refactor PatientController and PatientRequestDto to integrate AppConstants and validation groups for improved patient management

--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
@@ -1,11 +1,14 @@
 package com.patientmanagement.patientservice.controller;
 
+import com.patientmanagement.patientservice.config.AppConstants;
 import com.patientmanagement.patientservice.dto.PatientRequestDto;
 import com.patientmanagement.patientservice.dto.PatientResponseDTO;
+import com.patientmanagement.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.patientmanagement.patientservice.service.PatientService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,34 +25,38 @@ public class PatientController {
     }
 
     @GetMapping("/all-patients")
-    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients(
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_PATIENTS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
         List<PatientResponseDTO> patients = patientService.getAllPatients();
         return ResponseEntity.ok(patients);
     }
 
     @PostMapping("/add-patient")
-    public ResponseEntity<PatientResponseDTO> addPatient(@Valid @RequestBody PatientRequestDto patientRequestDto) {
+    public ResponseEntity<PatientResponseDTO> addPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDto patientRequestDto) {
         PatientResponseDTO responseDto = patientService.addPatient(patientRequestDto);
         return ResponseEntity.ok(responseDto);
     }
 
     @PutMapping("/edit-patient")
-    public ResponseEntity<PatientResponseDTO> editPatient(
-            @RequestParam(name = "username") String username,
-            @RequestBody PatientRequestDto patientRequestDto) {
+    public ResponseEntity<PatientResponseDTO> editPatient(@Validated(Default.class)
+                                                          @RequestParam(name = "username") String username,
+                                                          @RequestBody PatientRequestDto patientRequestDto) {
 
         PatientResponseDTO responseDto = patientService.updatePatient(username, patientRequestDto);
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("get-patient/{patientusername}")
+    @GetMapping("/get-patient/{patientusername}")
     public ResponseEntity<PatientResponseDTO> getPatientById(@PathVariable("patientusername") String patientusername) {
 
         PatientResponseDTO patient = patientService.getPatientByUsername(patientusername);
         return ResponseEntity.ok(patient);
     }
 
-    @DeleteMapping("delete-patient/{patientusername}")
+    @DeleteMapping("/delete-patient/{patientusername}")
     public ResponseEntity<PatientResponseDTO> deletePatient(@PathVariable("patientusername") String patientusername) {
 
         patientService.deletePatient(patientusername);
