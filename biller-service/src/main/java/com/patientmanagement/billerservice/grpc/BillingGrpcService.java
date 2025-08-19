0291557bb03fde535ff7c7997190605941aed714
Ashish jha <ajha5645@gmail.com>
2025-08-20 03:42:23 +0530

    implement createBillingAccount method in BillingGrpcService

--- biller-service/src/main/java/com/patientmanagement/billerservice/grpc/BillingGrpcService.java
+++ biller-service/src/main/java/com/patientmanagement/billerservice/grpc/BillingGrpcService.java
@@ -1,15 +1,25 @@
 package com.patientmanagement.billerservice.grpc;
 
 
+import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
 
     @Override
-    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> billingReponse){
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> billingReponse) {
+
+        log.info("createBillingAccount request for {}", billingRequest.toString());
+
+        BillingResponse billingResponse = BillingResponse.newBuilder().setAccountID("12345").setStatus("ACTIVE").build();
+
+        billingReponse.onNext(billingResponse);
+        billingReponse.onCompleted();
 
     }
 }
