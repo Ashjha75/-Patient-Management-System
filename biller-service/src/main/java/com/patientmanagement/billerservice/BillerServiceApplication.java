b4784c9f36770274f2db906019d776fafb6202b4
Ashish jha <ajha5645@gmail.com>
2025-08-20 01:35:23 +0530

    add annotation processing profile for biller-service and update encoding settings

---
+++ biller-service/src/main/java/com/patientmanagement/billerservice/BillerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.patientmanagement.billerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillerServiceApplication.class, args);
+        System.out.println("Working Fine Biller:🪼✨");
+    }
+
+}
