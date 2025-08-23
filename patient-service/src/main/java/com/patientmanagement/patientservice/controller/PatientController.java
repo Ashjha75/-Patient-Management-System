cc1f01fcf2748c77a94c5c38d70dc10350ad7a4d
Ashish jha <ajha5645@gmail.com>
2025-08-23 16:54:52 +0530

    remove unused import in PatientController and adjust formatting

--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/PatientController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/patients")
@@ -33,6 +32,7 @@ public class PatientController {
             @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
         PatientPageResponseDTO patients = patientService.getAllPatients(pageNumber, pageSize, sortBy, sortOrder);
         return ResponseEntity.ok(patients);
+
     }
 
     @PostMapping("/add-patient")
