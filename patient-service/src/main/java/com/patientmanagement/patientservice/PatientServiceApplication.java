5ecc3e7b254e15e47667ab1a7fcc2a2e0186d92c
Ashish jha <ajha5645@gmail.com>
2025-08-31 15:14:54 +0530

    add scheduling support to PatientServiceApplication; enhance UserRequestDto with validation and schema annotations

--- patient-service/src/main/java/com/patientmanagement/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/PatientServiceApplication.java
@@ -2,10 +2,12 @@ package com.patientmanagement.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
         net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration.class
 })
+@EnableScheduling
 public class PatientServiceApplication {
 
     public static void main(String[] args) {
--- patient-service/src/main/java/com/patientmanagement/patientservice/dto/UserRequestDto.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/dto/UserRequestDto.java
@@ -1,5 +1,20 @@
 package com.patientmanagement.patientservice.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserRequestDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @Schema(defaultValue = "user1")
+    private String username;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "Password must be atleast 8  characters")
+    @Schema(defaultValue = "password1")
+    private String password;
 }
