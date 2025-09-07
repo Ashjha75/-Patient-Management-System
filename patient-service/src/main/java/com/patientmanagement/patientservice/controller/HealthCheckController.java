5bb60c18fb8876379da5390a65167d1c31f366c3
Ashish jha <ajha5645@gmail.com>
2025-09-08 00:26:40 +0530

    feat: add health check endpoint and login redirect for OAuth2 authentication

--- patient-service/src/main/java/com/patientmanagement/patientservice/controller/HealthCheckController.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/controller/HealthCheckController.java
@@ -1,15 +1,23 @@
 package com.patientmanagement.patientservice.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
-@Tag(name = "1. Health Check",description = "Health check for Patient Service")
+@Tag(name = "1. Health Check", description = "Health check for Patient Service")
 public class HealthCheckController {
 
     @GetMapping("/health")
     public String healthCheck() {
         return "Health Is ok👌";
+    }
+
+    @GetMapping("/api/v1/login")
+    public void login(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/oauth2/authorization/google"); // or your provider
     }
 }
