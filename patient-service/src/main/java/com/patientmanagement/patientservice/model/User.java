0518fbe89fa285c3daa54fd389daa47099662729
Ashish jha <ajha5645@gmail.com>
2025-09-10 21:14:21 +0530

    feat: implement UserDetails interface in User class for enhanced security and role management

--- patient-service/src/main/java/com/patientmanagement/patientservice/model/User.java
+++ patient-service/src/main/java/com/patientmanagement/patientservice/model/User.java
@@ -7,9 +7,14 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users",
@@ -24,7 +29,7 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -72,6 +77,13 @@ public class User {
         this.email = email;
         this.password = password;
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toSet());
     }
 
 
